badbfed93a45eb55e6349a9436c95994ff2b36a2
peti333 <makkpetiek@gmail.com>
2025-03-11 13:12:56 +0100

    Lecture

---
+++ Lecture/1.types.py
@@ -0,0 +1,21 @@
+# Types
+
+szam = 5
+szo = "asd"
+igazVagyHamis = True
+
+print(szam)
+print(szo)
+print(igazVagyHamis)
+print(4 == 5)
+
+a = 4
+b = 5
+print(a + b)
+
+
+lista = [1,2,3,5,7]
+lista2 = ["Hello", "World", "!"]
+
+print(lista[0])
+print(lista2[1])
