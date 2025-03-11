6ecf66635d1a410574a5f5e5f350dafd87e9c91f
peti333 <makkpetiek@gmail.com>
2025-03-11 12:26:43 +0100

    Logical test

---
+++ Test/test3.py
@@ -0,0 +1,14 @@
+#
+#   Valósítsd meg a logikai táblázatot
+#
+
+#|      |  a I |  a H |
+#| b I  |   I  |   I  |
+#| b H  |   I  |   H  |
+#
+
+def Solution(a,b):
+    return not a and b
+
+
+print(str(Solution(True,True)))
