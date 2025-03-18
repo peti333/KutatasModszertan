d3676c2bc3ac2bbe684bd44ee77940c4b906ce3a
fegyobeno <57350206+fegyobeno@users.noreply.github.com>
2025-03-18 10:55:39 +0100

    Create beepitett_operatorok.py
    created a file fror learning operators and such

---
+++ Lecture/beepitett_operatorok.py
@@ -0,0 +1,33 @@
+a = 12
+b =20
+c = "almafa"
+l = [1,2,3,4,5]
+
+print(sum(a,b))
+print(avg(a,b))
+print(sd(a,b))
+
+print(sum(l))
+print(avg(l))
+print(sd(l))
+
+print(len(c))
+
+print(len(str(a)))
+
+'''
+a+b
+a-b
+a*b
+a/b
+a%b
+a**b
+
+concatenation of two strings
+"a" + "b"
+"a" + str(5)
+
+operations between strings and integers
+"a"*5 -> aaaaa
+"a" + 5 -> error
+'''
