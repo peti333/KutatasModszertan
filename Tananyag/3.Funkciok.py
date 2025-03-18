157764717cbc5fe57da616a041c16bf303cd05b6
Palko <palko@horogszegi.hu>
2025-03-18 13:26:16 +0100

    add maximumKivalasztas

--- Tananyag/3.Funkciok.py
+++ Tananyag/3.Funkciok.py
@@ -1,8 +1,21 @@
 # függvények, újra használható kód
 
-def plus(a,b):
+def plus(a, b):
     return a + b
 
-print(plus(1,2))
-print(plus(6,-2))
 
+print(plus(1, 2))
+print(plus(6, -2))
+
+# konkrét függvény, legnagyobb elem kiválasztása
+lista = [3, 65, -1, 10, 1]
+
+def maximumKivalasztas(listaParameter):
+    eddigiLegnagyobb = listaParameter[0]
+    for i in listaParameter:
+        if i > eddigiLegnagyobb:
+            eddigiLegnagyobb = i
+
+    return eddigiLegnagyobb
+
+print('maximum: ', maximumKivalasztas(lista))
